6f6ff84942319bd0a46abb2d80272736b3505c40
Nikolay Dolgopolov <dolgopolov.n.v@gmail.com>
2017-05-18 09:47:24 +0700

    Initial Commit

---
+++ iosTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosTest
+//
+//  Created by ndolgopolov on 18.05.17.
+//  Copyright © 2017 ndolgopolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosTest
+//
+//  Created by ndolgopolov on 18.05.17.
+//  Copyright © 2017 ndolgopolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
